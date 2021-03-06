1e8e3e06d57c3765df4c48b187372ea5babf5ff2
Martin K <martinkz@users.noreply.github.com>
2021-03-06 18:03:13 +0000

    Initial commit

---
+++ src/index.php
@@ -0,0 +1,14 @@
+<?php
+
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+namespace martinkzh\PrismicCacheTool
+
+class SetUpCache
+{
+    private static function init()
+    {
+    	echo "test";
+    }
+}
